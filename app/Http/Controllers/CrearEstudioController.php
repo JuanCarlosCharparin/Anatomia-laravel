1848cc29c63fcc57c2b952392917fe6648360ac9
Juan Carlos Charparin  pcHU <jccharparin@gmail.com>
2024-08-01 08:30:49 -0300

    Agregamos codigo_nomenclador al crear estudio

--- app/Http/Controllers/CrearEstudioController.php
+++ app/Http/Controllers/CrearEstudioController.php
@@ -8,8 +8,9 @@ use App\Models\Personal;
 use App\Models\Profesional;
 use App\Models\Especialidad;
 use App\Models\Servicio;
+use App\Models\CodigoNomencladorAP;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB; // Importa la fachada DB
+use Illuminate\Support\Facades\DB;
 
 class CrearEstudioController extends Controller
 {
@@ -70,7 +71,7 @@ class CrearEstudioController extends Controller
         // Generar el nuevo número de servicio
         $newServicioNumber = $lastEstudio ? $lastEstudio->nro_servicio + 1 : 1;
 
-        $professionals = Paciente::getProfessionals();
+        $professionals = Paciente::getProfessionals(); // Asumí que obtienes todos los profesionales aquí
         $servicios = Especialidad::getServicio();
 
         // Pasar los datos a la vista
@@ -101,70 +102,93 @@ class CrearEstudioController extends Controller
                 }
             }],
             'profesional_salutte_id' => 'required|exists:profesional,profesional_salutte_id',
+            'codigos' => 'nullable|array',
+            'codigos.*' => 'nullable|string|max:255'
         ]);
 
-        // Obtener el ID del servicio desde la base de datos secundaria
-        $servicioSalutteId = $validatedData['servicio_salutte_id'];
-        $servicio = Especialidad::findServicioById($servicioSalutteId);
+        DB::beginTransaction();
 
-        if (!$servicio) {
-            return redirect()->back()->with('error', 'Servicio no encontrado.');
+        try {
+            // Obtener el ID del servicio desde la base de datos secundaria
+            $servicioSalutteId = $validatedData['servicio_salutte_id'];
+            $servicio = Especialidad::findServicioById($servicioSalutteId);
+
+            if (!$servicio) {
+                return redirect()->back()->with('error', 'Servicio no encontrado.');
+            }
+
+            // Crear o actualizar el servicio en la base de datos principal
+            $servicioDb = Servicio::updateOrCreate(
+                ['servicio_salutte_id' => $servicioSalutteId],
+                ['nombre_servicio' => $servicio->nombre_servicio]
+            );
+
+            // Obtener el ID del servicio en la base de datos principal
+            $servicioId = $servicioDb->id;
+
+            // Obtener el DNI del paciente desde la base de datos secundaria
+            $documento = $validatedData['documento'];
+            $paciente = Paciente::findByDni($documento);
+
+            // Crear o actualizar el paciente en la base de datos principal
+            $pacienteDb = Personal::updateOrCreate(
+                ['documento' => $paciente->documento],
+                [
+                    'persona_salutte_id' => $paciente->id,
+                    'nombres' => $paciente->nombres,
+                    'apellidos' => $paciente->apellidos,
+                    'obra_social' => $paciente->obra_social,
+                    'fecha_nacimiento' => $paciente->fecha_nacimiento,
+                    'genero' => $paciente->genero,
+                ]
+            );
+
+            // Obtener el ID del paciente en la base de datos principal
+            $pacienteId = $pacienteDb->id;
+
+            // Verificar el ID del profesional en la base de datos principal
+            $profesionalSalutteId = $validatedData['profesional_salutte_id'];
+            $profesional = Profesional::where('profesional_salutte_id', $profesionalSalutteId)->first();
+
+            if (!$profesional) {
+                return redirect()->back()->with('error', 'El profesional seleccionado no existe en la base de datos.');
+            }
+
+            $profesionalId = $profesional->id;
+
+            $estudio = Estudio::updateOrCreate([
+                    'nro_servicio' => $validatedData['nro_servicio'],
+                    'estado_estudio' => 'creado',
+                    'tipo_estudio_id' => $validatedData['tipo_estudio'],
+                    'diagnostico_presuntivo' => $validatedData['diagnostico'],
+                    'fecha_carga' => $validatedData['fecha_carga'],
+                    'medico_solicitante' => $validatedData['medico_solicitante'],
+                    'servicio_id' => $servicioId,
+                    'personal_id' => $pacienteId,
+                    'profesional_id' => $profesionalId,
+                ]);
+
+            $nro_servicio = $validatedData['nro_servicio'];
+            $codigos_nomenclador = $validatedData['codigos'] ?? [];
+
+            if (!empty($codigos_nomenclador)) {
+                $codigosToInsert = [];
+                foreach ($codigos_nomenclador as $codigo) {
+                    if (!empty($codigo)) {
+                        $codigosToInsert[] = ['nro_servicio' => $nro_servicio, 'codigo' => $codigo];
+                    }
+                }
+                // Inserta o actualiza los códigos en batch
+                CodigoNomencladorAP::upsert($codigosToInsert, ['nro_servicio', 'codigo']);
+            }
+
+            DB::commit();
+
+            return redirect()->route('estudios.index')->with('success', 'Estudio creado exitosamente.');
+        } catch (Exception $e) {
+            DB::rollBack();
+            return redirect()->route('estudios.index')->with('error', 'Ocurrió un error al crear el estudio: ' . $e->getMessage());
         }
-
-        // Crear o actualizar el servicio en la base de datos principal
-        $servicioDb = Servicio::updateOrCreate(
-            ['servicio_salutte_id' => $servicioSalutteId],
-            ['nombre_servicio' => $servicio->nombre_servicio]
-        );
-
-        // Obtener el ID del servicio en la base de datos principal
-        $servicioId = $servicioDb->id;
-
-        // Obtener el DNI del paciente desde la base de datos secundaria
-        $documento = $validatedData['documento'];
-        $paciente = Paciente::findByDni($documento);
-
-        // Crear o actualizar el paciente en la base de datos principal
-        $pacienteDb = Personal::updateOrCreate(
-            ['documento' => $paciente->documento], // Usa el DNI como identificador
-            [
-                'persona_salutte_id' => $paciente->id,
-                'nombres' => $paciente->nombres,
-                'apellidos' => $paciente->apellidos,
-                'obra_social' => $paciente->obra_social,
-                'fecha_nacimiento' => $paciente->fecha_nacimiento,
-                'genero' => $paciente->genero,
-            ]
-        );
-
-        // Obtener el ID del paciente en la base de datos principal
-        $pacienteId = $pacienteDb->id;
-
-        // Verificar el ID del profesional en la base de datos principal
-        $profesionalSalutteId = $validatedData['profesional_salutte_id'];
-        $profesional = Profesional::where('profesional_salutte_id', $profesionalSalutteId)->first();
-
-        if (!$profesional) {
-            return redirect()->back()->with('error', 'El profesional seleccionado no existe en la base de datos.');
-        }
-
-        $profesionalId = $profesional->id;
-
-        // Crear el nuevo estudio y usar el ID del servicio
-        $estudio = Estudio::create([
-            'nro_servicio' => $validatedData['nro_servicio'],
-            'estado_estudio' => 'creado',
-            'tipo_estudio_id' => $validatedData['tipo_estudio'],
-            'diagnostico_presuntivo' => $validatedData['diagnostico'],
-            'fecha_carga' => $validatedData['fecha_carga'],
-            'medico_solicitante' => $validatedData['medico_solicitante'],
-            'servicio_id' => $servicioId,
-            'personal_id' => $pacienteId,
-            'profesional_id' => $profesionalId,
-        ]);
-
-        // Redirigir o devolver respuesta
-        return redirect()->route('estudios.index')->with('success', 'Estudio creado exitosamente.');
     }
 
     // Función para buscar paciente
@@ -176,18 +200,5 @@ class CrearEstudioController extends Controller
         $patients = Paciente::search($searchTerm);
 
         return response()->json($patients);
-    }
-
-    // Función para buscar profesional
-    public function searchProfessional(Request $request)
-    {
-        // Puedes cambiar estos IDs o obtenerlos dinámicamente según tus necesidades
-        $professionalIds = [198041, 106780]; 
-    
-        // Consultar profesionales usando el modelo Paciente
-        $professionals = Paciente::searchProfessional($professionalIds);
-    
-        // Asegúrate de que 'professionals' es una variable que existe en la vista
-        return view('estudios.create', compact('professionals'));
     }
 }
--- app/Models/CodigoNomencladorAP.php
+++ app/Models/CodigoNomencladorAP.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,12 +10,12 @@ class CodigoNomencladorAP extends Model
     use HasFactory;
 
     protected $table = 'codigo_nomenclador_ap';
+
+    // Si no usas timestamps en esta tabla
     public $timestamps = false;
 
     protected $fillable = [
         'nro_servicio',
         'codigo',
     ];
-
-    public $timestamps = false; // Si no usas timestamps en esta tabla
 }
